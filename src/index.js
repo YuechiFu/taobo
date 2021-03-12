55b0a961ff5b27d446f6825a60815d7a71cba7f8
huchuanwei <chuanwei.hu@seeed.cc>
2021-03-12 18:31:41 +0800

    12123

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const Vue  = require('../node_modules/vue/dist/vue.js');
+
 let userData = require('./mockData/userData.js');
 let productData = require('./mockData/productData.js')
+
 new Vue({
     data(){
         return {
